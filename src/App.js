ef0555e4655288cc76ad26cbe1fc9a0bf3a634a6
Hayen Phan <hayenphan@outlook.com>
2020-01-25 19:10:45 +0100

    Fetched items

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          error: null,
+          isLoaded: false,
+          items: []
+      }
+    }
+
+    componentDidMount() {
+        fetch("http://145.24.222.245:8000/instruments")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    })
+                },
+
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    })
+                }
+            )
+    }
+
+    render() {
+
+        const { error, isLoaded, items } = this.state;
+        const itemsi = this.state.items.items;
+        if (error) {
+            return <div> Error: {error.message} </div>;
+        } else if (!isLoaded) {
+            return <div> Loading... </div>;
+        } else {
+            return (
+              <ul>
+
+              {itemsi.map(item => (
+                 <li key={item.id}>
+                   {item.name}
+                 </li>
+              ))}
+
+              </ul>
+            )
+        }
+    }
 }
 
 export default App;
