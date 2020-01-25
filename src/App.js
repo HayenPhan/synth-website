7275641b2bfcd5b92db5d239feb2202733ca0c24
Hayen Phan <hayenphan@outlook.com>
2020-01-25 19:49:18 +0100

    WIP adding detail

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import styled from 'styled-components';
+import Modal from "react-bootstrap/Modal";
+
+const StyledLi = styled.li`
+  font-family: Arial;
+  font-size: 20px
+`
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+`
+
+const LiWrapper = styled.div`
+    margin-bottom: 25px;
+`
 
 class App extends React.Component {
     constructor(props) {
@@ -7,7 +23,10 @@ class App extends React.Component {
       this.state = {
           error: null,
           isLoaded: false,
-          items: []
+          items: [],
+          modalShow: false,
+          user: '',
+          teacher: '',
       }
     }
 
@@ -31,6 +50,12 @@ class App extends React.Component {
             )
     }
 
+    onHide = () => this.setState({ modalShow: false });
+
+    showModal = (user, teacher) => {
+        this.setState({user: user, teacher: teacher}, ()=> this.setState({ modalShow: true }));
+    }
+
     render() {
 
         const { error, isLoaded, items } = this.state;
@@ -40,16 +65,31 @@ class App extends React.Component {
         } else if (!isLoaded) {
             return <div> Loading... </div>;
         } else {
+
+                console.log(itemsi)
             return (
-              <ul>
+              <StyledUl>
 
-              {itemsi.map(item => (
-                 <li key={item.id}>
-                   {item.name}
-                 </li>
-              ))}
 
-              </ul>
+                  {itemsi.map((item, i) => {
+
+                    //const user = item.user[i];
+                    //const teacher = item.teacher[i];
+
+                    return (
+                      <LiWrapper key={i}>
+                           <StyledLi>
+                             {item.name}
+                           </StyledLi>
+                           <Button onClick={() => this.showModal()}> Detail </Button>
+                           <Modal show={this.state.modalShow} onHide={this.onHide}>
+                           </Modal>
+                       </LiWrapper>
+                    )
+
+                  })
+                  }
+              </StyledUl>
             )
         }
     }
