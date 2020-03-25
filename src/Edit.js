2179ac2b12d44f7fc0978b72b6fb0f39e05e745b
Hayen Phan <hayenphan@outlook.com>
2020-03-25 23:01:19 +0100

    Created edit page

--- src/Edit.js
+++ src/Edit.js
@@ -1,26 +1,107 @@
 import React from 'react';
-import { Container, Row, Col, Form, Button, Table, ListGroup} from 'react-bootstrap';
+import { Card, Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-class Edit extends React.Component {
+class Detail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            item: null,
-            message: null
+            items: null,
+            message: null,
+            id: '',
+            name: '',
+            user: '',
+            teacher: '',
+            loading: false
         }
     }
 
     async componentDidMount() {
-        
+        this.getAll();
+    }
+
+    getAll = async () =>  {
+        const { id } = this.props.match.params
+
+        const res = await fetch(`http://145.24.222.245:8000/instruments/${id}`)
+        const data = await res.json()
+
+        this.setState({
+           items: [data]
+        })
+
+        this.setState({
+            loading: true,
+            id: data._id,
+            name: data.name,
+            user: data.user,
+            teacher: data.teacher
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        fetch(`http://145.24.222.245:8000/instruments/${this.state.id}`, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: e.target.name.value,
+                user: e.target.user.value,
+                teacher: e.target.teacher.value
+            })
+        }).then(res => {
+            if (res.ok) {
+                this.setState({
+                    message: 'Gegevens zijn gewijzigd'
+                })
+            }
+        })
     }
 
     render() {
         return (
             <div>
-                 sadsdsadasdsa
+                <h2> Gegevens wijzigen van student </h2>
+                {
+                    this.state.message ? (
+                        <p>{this.state.message}</p>
+                    ) : (null)
+                }
+                {
+                    !this.state.loading ? (
+                        <p>Aan het laden</p>
+                    ) : (
+
+                        <form onSubmit={this.handleSubmit}>
+                            <Form.Group controlId="formGroupName">
+                              <Form.Label>Instrument</Form.Label>
+                              <Form.Control name="name" value={this.state.name} onChange={e => this.setState({ name: e.target.value })} />
+                            </Form.Group>
+
+                            <Form.Group controlId="formGroupUser">
+                              <Form.Label>Leerling</Form.Label>
+                              <Form.Control name="user" value={this.state.user} onChange={e => this.setState({ user: e.target.value })} />
+                            </Form.Group>
+
+                            <Form.Group controlId="formGroupTeacher">
+                              <Form.Label>Docent</Form.Label>
+                              <Form.Control name="teacher" value={this.state.teacher} onChange={e => this.setState({ teacher: e.target.value })} />
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit">
+                                Wijzigen
+                           </Button>
+
+                      </form>
+                    )
+                }
             </div>
         )
     }
 }
 
-export default Edit;
+export default Detail;
